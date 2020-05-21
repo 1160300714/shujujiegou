b1a670a5a6818829154344928d4394e6823c10b8
jyri <694515124@qq.com>
2020-05-21 11:43:33 +0800

    update

--- linklist.c
+++ linklist.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct{
 	int data;
 	struct lnode *next;
 }lnode;
 
-int int main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-	lnode l;
-	l
+	lnode *head,*p,*q,*demo;
+	head = NULL;
+
+	p = (lnode*)malloc(sizeof(lnode));
+	q = (lnode*)malloc(sizeof(lnode));
+	p->data = 6;
+	p->next = q;
+
+	q->data = 8;
+	q->next = NULL;
+
+	
+	
+	head = p;
+	
 
 
-
+	demo = head;
+	while(demo!=NULL){
+		printf("%d\n", demo->data );
+		demo = demo->next;
+	}
 
 
 

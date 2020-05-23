ca44ea0d48f321f17dbef4df7828bbd0d8cbdccf
jyri <694515124@qq.com>
2020-05-23 09:02:45 +0800

    update

--- linklist.c
+++ linklist.c
@@ -6,31 +6,53 @@ typedef struct{
 	struct lnode *next;
 }lnode;
 
+int output(struct londe *l){
+	lnode *print = l;
+	while(print!=NULL){
+		printf("%d\n", print->data );
+		print = print->next;
+	}
+	return 1;
+}
+
+lnode *getelem(struct lnode *l ,int i){//查找节点
+	int j = 1;
+	struct lnode *p;
+	p = l->next;
+	//错误处理
+	while(j<i){
+		p = p->next;
+		j++;
+	};
+	return p;
+}
+
 int main(int argc, char const *argv[])
 {
-	lnode *head,*p,*q,*demo;
+	lnode *head,*a,*b,*c,*d;
+	lnode *print;//打印
 	head = NULL;
 
-	p = (lnode*)malloc(sizeof(lnode));
-	q = (lnode*)malloc(sizeof(lnode));
-	p->data = 6;
-	p->next = q;
+	a = (lnode*)malloc(sizeof(lnode));
+	b = (lnode*)malloc(sizeof(lnode));
+	c = (lnode*)malloc(sizeof(lnode));
+	d = (lnode*)malloc(sizeof(lnode));
 
-	q->data = 8;
-	q->next = NULL;
+	a->data = 6;
+	a->next = b;
 
-	
-	
-	head = p;
-	
+	b->data = 8;
+	b->next = c;
 
+	c->data = 9;
+	c->next = d;
 
-	demo = head;
-	while(demo!=NULL){
-		printf("%d\n", demo->data );
-		demo = demo->next;
-	}
+	d->data = 11;
+	d->next = NULL;
 
+	head = a;//头指针赋值
+
+	output(head);
 
 
 
